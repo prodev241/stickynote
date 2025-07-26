577b1dbd008e1dc622cc152a344efe6231f64666
prodev241 <qasi2345ar@gmail.com>
2025-07-26 19:58:44 +0500

    update routes

--- src/pages/ViewAll.jsx
+++ src/pages/ViewAll.jsx
@@ -1,35 +1,33 @@
 const ViewAll = () => {
   return (
     <>
-    <div className=" bg-gray-300 h-screen">
- <h1 className="font-bold text-2xl pl-4 pt-2 text-amber-800">Veiw All</h1>
-    <ul className="flex  gap-4 mt-4  pl-4">
-    <li  className=" bg-amber-100 rounded border-amber-600 border-t-amber-200 border-b-amber-400 w-[200px] h-[190px] p-2">
-        <p>hello world</p>
-
-    </li>
-<li  className=" bg-amber-100 rounded border-amber-600 border-t-amber-200 border-b-amber-400 w-[200px] h-[190px] p-2">
-        <p>Today task is about react router</p>
-    </li>
-    <li  className=" bg-amber-100 rounded border-amber-600 border-t-amber-200 border-b-amber-400 w-[200px] h-[190px] p-2">
-        <p>Today task is about react router</p>
-    </li>
-    <li  className=" bg-amber-100 rounded border-amber-600 border-t-amber-200 border-b-amber-400 w-[200px] h-[190px] p-2">
-        <p>Today task is about react router</p>
-    </li>
-    <li  className=" bg-amber-100 rounded border-amber-600 border-t-amber-200 border-b-amber-400 w-[200px] h-[190px] p-2">
-        <p>Today task is about react router</p>
-    </li>
-    <li  className=" bg-amber-100 rounded border-amber-600 border-t-amber-200 border-b-amber-400 w-[200px] h-[190px] p-2">
-        <p>Today task is about react router</p>
-    </li>
-    </ul>
-    </div>
-   
+      <div className=" bg-gray-300 h-screen">
+        <h1 className="font-bold text-2xl pl-4 pt-2 text-amber-800">
+          Veiw All
+        </h1>
+        <ul className="flex  gap-4 mt-4  pl-4">
+          <li className=" bg-amber-100 rounded border-amber-600 border-t-amber-200 border-b-amber-400 w-[200px] h-[190px] p-2">
+            <p>hello world</p>
+          </li>
+          <li className=" bg-amber-100 rounded border-amber-600 border-t-amber-200 border-b-amber-400 w-[200px] h-[190px] p-2">
+            <p>Today task is about react router</p>
+          </li>
+          <li className=" bg-amber-100 rounded border-amber-600 border-t-amber-200 border-b-amber-400 w-[200px] h-[190px] p-2">
+            <p>Today task is about react router</p>
+          </li>
+          <li className=" bg-amber-100 rounded border-amber-600 border-t-amber-200 border-b-amber-400 w-[200px] h-[190px] p-2">
+            <p>Today task is about react router</p>
+          </li>
+          <li className=" bg-amber-100 rounded border-amber-600 border-t-amber-200 border-b-amber-400 w-[200px] h-[190px] p-2">
+            <p>Today task is about react router</p>
+          </li>
+          <li className=" bg-amber-100 rounded border-amber-600 border-t-amber-200 border-b-amber-400 w-[200px] h-[190px] p-2">
+            <p>Today task is about react router</p>
+          </li>
+        </ul>
+      </div>
     </>
-      
-    
-  )
-}
+  );
+};
 
 export default ViewAll;
